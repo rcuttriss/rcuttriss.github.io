b2af0ea611d5f58edd91ec744da6901294621d3e
rcuttriss <34427457+rcuttriss@users.noreply.github.com>
2022-05-08 20:52:30 -0700

    Delete udita.js

--- udita.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    if(window.scrollY > 200) {
-        document.getElementById("headerContainer").style.background = "var(--alt-red-color)";
-        document.getElementById("headerContainer").style.color = "white";
-    } else {
-        document.getElementById("headerContainer").style.background = "var(--main-dark-color)";
-        document.getElementById("headerContainer").style.color = "var(--alt-red-color)";
-    }
-});
